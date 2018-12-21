72c7349dedd2d766150a80ae788d4c3f236b10a6
paulapivat <paul.apivat@gmail.com>
2018-12-21 14:52:17 +0700

    update About page

--- client/src/components/About.js
+++ client/src/components/About.js
@@ -9,37 +9,39 @@ class About extends React.Component {
 
     render(){
 
-        const data = [
-            {
-                data: {
-                battery: 0.7,
-                design: .8,
-                useful: 0.9,
-                speed: 0.67,
-                weight: 0.8
-                },
-                meta: { color: 'blue' }
-            },
-            {
-                data: {
-                battery: 0.6,
-                design: .85,
-                useful: 0.5,
-                speed: 0.6,
-                weight: 0.7
-                },
-                meta: { color: 'red' }
-            }
-            ];
+        // EXAMPLE of data array; captions object (size number)
+        // no need in render
+        // const data = [
+        //     {
+        //         data: {
+        //         battery: 0.7,
+        //         design: .8,
+        //         useful: 0.9,
+        //         speed: 0.67,
+        //         weight: 0.8
+        //         },
+        //         meta: { color: 'blue' }
+        //     },
+        //     {
+        //         data: {
+        //         battery: 0.6,
+        //         design: .85,
+        //         useful: 0.5,
+        //         speed: 0.6,
+        //         weight: 0.7
+        //         },
+        //         meta: { color: 'red' }
+        //     }
+        //     ];
         
-            const captions = {
-            // columns
-            battery: 'Battery Capacity',
-            design: 'Design',
-            useful: 'Usefulness',
-            speed: 'Speed',
-            weight: 'Weight'
-        };
+        //     const captions = {
+        //     // columns
+        //     battery: 'Battery Capacity',
+        //     design: 'Design',
+        //     useful: 'Usefulness',
+        //     speed: 'Speed',
+        //     weight: 'Weight'
+        // };
 
 
         return (
@@ -55,18 +57,30 @@ class About extends React.Component {
                     }}
                     data={[
                         // data
+                        // each chart is it's own object with two keys: data & meta
                         {
-                        data: {
-                            battery: 0.7,
-                            design: .8,
-                            useful: 0.9,
-                            speed: 0.67,
-                            weight: 0.8
+                            data: {
+                                battery: 0.7,
+                                design: .8,
+                                useful: 0.9,
+                                speed: 0.67,
+                                weight: 0.8
+                            },
+                            meta: { color: '#58FCEC' }
                         },
-                        meta: { color: '#58FCEC' }
+                        {
+                            data: {
+                                battery: 0.3,
+                                design: .4,
+                                useful: 0.5,
+                                speed: 0.9,
+                                weight: 0.1
+                            },
+                            meta: { color: 'red' }
                         },
                     ]}
-                    size={400}
+                    // chart size
+                    size={800}
                 />
             </div>
         )
